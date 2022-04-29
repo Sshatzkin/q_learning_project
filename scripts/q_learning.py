acbf3f08ce76f9fab2876d097dd885b975606088
pancakefries <jagarza723@gmail.com>
2022-04-29 18:16:31 -0500

    initialization and preliminary random action code

--- scripts/q_learning.py
+++ scripts/q_learning.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
+from ast import NameConstant
 import rospy
 import numpy as np
 import os
-from q_learning.msg import QMatrix, QLearningReward, RobotMoveObjectToTag
+from q_learning_project.msg import QMatrix, QLearningReward, RobotMoveObjectToTag, QMatrixRow
 
 # Path of directory on where this file is located
 path_prefix = os.path.dirname(__file__) + "/action_states/"
@@ -48,28 +49,40 @@ class QLearning(object):
 
         # --------------- Our Code after this point --------------- #
         # Publish Q Matrix
-        self.particles_pub = rospy.Publisher("/q_learning/q_matrix", QMatrix, queue_size=10)
+        self.matrix_pub = rospy.Publisher("/q_learning/q_matrix", QMatrix, queue_size=10)
 
         # Publish Robot Action
-        self.particles_pub = rospy.Publisher("/q_learning/robot_action", RobotMoveObjectToTag, queue_size=10)
+        self.action_pub = rospy.Publisher("/q_learning/robot_action", RobotMoveObjectToTag, queue_size=10)
 
         # subscribe to the reward received from the reward node
         rospy.Subscriber("/q_learning/reward", QLearningReward, self.q_learning_reward_recieved)
 
         self.converged = False
 
-        # TODO: Initialize Q Matrix of size 64 (states) x 9 (actions) and publish it
+        # Initialize Q Matrix of size 64 (states) x 9 (actions) and publish it
+        n_actions = 9; n_states = 64
+        self.q_matrix = self.initialize_q_matrix(n_states, n_actions)
+        self.matrix_pub.publish(self.q_matrix)
 
-        # TODO: Publish first random action
+        # Publish first random action
+        rand_a = self.random_action()
+        self.action_pub.publish(rand_a)
+
+    def initialize_q_matrix(self, states, actions):
+        empty_matrix = np.zeros((states, actions))
+        q_matrix = QMatrix(q_matrix=QMatrixRow(empty_matrix))
+        return q_matrix
 
     def save_q_matrix(self):
         # TODO: You'll want to save your q_matrix to a file once it is done to
         # avoid retraining
         return
 
-    def random_action():
-      # TODO : Pick a random action given current state and action matrix
-      return
+    def random_action(self):
+        # TODO: Pick a random action given current state and action matrix
+        # TODO: No clue if I coded this right, have to check format of msg
+        i = np.random.randint(low=0, high=(len(self.actions) - 1))
+        return self.actions[i]
 
     def q_learning_reward_recieved(self, reward_msg):
       
